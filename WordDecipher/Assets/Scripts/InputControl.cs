e5df815ab2f8e9f54ad41aa177ab5c2368f08c1b
Rengre <renata.grela6@gmail.com>
2020-05-24 23:04:42 +0200

    adding files

---
+++ WordDecipher/Assets/Scripts/InputControl.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * This class implements methods used by input field and hint button
+ * Ta klasa implementuje metody używane przez pole wprowadzania i przycisk podpowiedzi
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputControl : MonoBehaviour
+{
+    [SerializeField] private InputField input;//represents field where user writes his answer
+    [SerializeField] private Text text;// represents a text field with an encrypted word
+    public GameObject nextLevel;//represents button NEXTLEVEL
+    public GameObject quitPanel;//represents panel activated when the user wins the game
+    public GameObject hintPanel;//represents panel with hint
+    public GameObject imageHint;//represents image object in panel with hint
+    public GameObject imageAlfabet;//represents image object in panel with hint
+    public Text texthint;//represents text object in panel with hint
+    public AudioSource music;//represents music activated when the user wins the game
+
+    public void GetInput(string answer)//method used by input field
+    {
+        if (answer != text.GetComponent<GenarateText>().word) //check that user set wrong answer
+        {
+            input.text = null;
+            input.placeholder.GetComponent<Text>().text = "Próbuj dalej";
+        }else
+        {
+
+            if (text.GetComponent<GenarateText>().level <6)
+            {
+                input.text = null;
+                input.placeholder.GetComponent<Text>().text = "Brawo!!";
+                nextLevel.SetActive(true);
+            }
+            else 
+            { 
+                text.text = "Gratulacje !!";
+                nextLevel.SetActive(false);
+                quitPanel.SetActive(true);
+                music.Play();
+                
+            }
+           
+        }
+    }
+    public void Hint()//method that sets visibility of panel with hint
+    {
+        hintPanel.SetActive(true);
+        if (text.GetComponent<GenarateText>().caeserGame)
+        {
+            texthint.text = "Szyfr podstawieniowy\n Każda litera tekstu została zastępiona przez inną literę, oddaloną od niej o stałą liczbę pozycji w alfabecie";
+            imageAlfabet.SetActive(true);
+            imageHint.SetActive(true);
+        }
+        if (text.GetComponent<GenarateText>().shiftGame)
+        {
+            texthint.text = "Szyfr przestawieniowy \n Litery w zaszyfrowanym słowie zostały poprzestawiane. \n Sąsiadujące litery zamieniły się miejscami.  ";
+            imageAlfabet.SetActive(false);
+            imageHint.SetActive(false);
+        }
+    } 
+    public void ExitHint()//method that closes panel with hint
+    {
+        hintPanel.SetActive(false);
+    }
+
+}
---
+++ WordDecipher/Assets/Scripts/Pausemenu.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * This class activates the panel with buttons RESUME,MENU,QUIT and implements methods used by these buttons
+ * Ta klasa aktywuje panel z przyciskami RESUME, MENU, QUIT i implementuje metody używane przez te przyciski
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausemenu : MonoBehaviour
+{
+
+    public static bool gameispaused = false; //represents that panel with buttons is visible 
+    public GameObject pausemenuui; // this object represent panel with buttons
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) //check if key ESC is pressed
+        {
+            if (gameispaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+   public void Resume() //method that set panel invisible
+    {
+        pausemenuui.SetActive(false);
+        gameispaused = false;
+    }
+    void Pause() //method that set panel visible
+    {
+       
+        pausemenuui.SetActive(true);
+        gameispaused = true;
+    }
+    public void Quitgame() //method that closes the game activated by the button QUIT
+    {
+        Application.Quit();
+    }
+    public void RestartGame()//method that restart the game activated by the button MENU
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
